fa2f58a409b730df06fa46e2dd039af7f197c4e1
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-03-05 14:23:40 +0100

    EoD
    14:23 5/3/2026
    De HTML pagina's zijn hernoemt naar PHP.

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>STRAAT Arnhem</title>
+  <title>STRAAT | Arnhem</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="img/logo.png" type="image/png" alt="STRAAT logo">
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,300;0,400;0,600;0,700;0,900;1,400&display=swap" rel="stylesheet" />
